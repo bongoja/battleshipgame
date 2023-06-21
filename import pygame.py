d9045997cf2d6ec4289b4765d07f3e3242f86f7f
majekbezjajek <tpkorepetycje@gmail.com>
2023-06-21 22:57:09 +0200

    dodane 4 plansze
    lol dziala

--- import pygame.py
+++ import pygame.py
@@ -5,12 +5,26 @@ pygame.init()
 
 pygame.display.set_caption("Gra w statki")
 
-SZEROKOSC = 1200
-WYSOKOSC = 800
+ROZMIAR_PLANSZY = 45
+POZIOMY_MARGINES = ROZMIAR_PLANSZY * 4
+PIONOWY_MARGINES = ROZMIAR_PLANSZY 
+
+SZEROKOSC = ROZMIAR_PLANSZY * 10 * 2 + POZIOMY_MARGINES
+WYSOKOSC = ROZMIAR_PLANSZY * 10 * 2 + PIONOWY_MARGINES
 
 EKRAN = pygame.display.set_mode((SZEROKOSC, WYSOKOSC))
 
 NIEBIESKI = (176, 196, 222)
+SZARY = (112, 128, 144)
+
+#funkcja która rysuje kratkę
+def draw_grid(left = 0, top = 0):
+    for i in range(100):
+        x = left + i % 10 * ROZMIAR_PLANSZY
+        y = top + i // 10 * ROZMIAR_PLANSZY
+        PLANSZA = pygame.Rect(x, y, ROZMIAR_PLANSZY, ROZMIAR_PLANSZY)
+        pygame.draw.rect(EKRAN, SZARY, PLANSZA, width = 3)
+
 
 animating=True
 pausing=False
@@ -34,6 +48,21 @@ while animating:
 
         if not pausing:
             EKRAN.fill(NIEBIESKI)
+
+        #rysowanie ,,szukającej'' planszy
+        draw_grid()
+        draw_grid(left = (SZEROKOSC-POZIOMY_MARGINES)//2 + POZIOMY_MARGINES, top = (WYSOKOSC-PIONOWY_MARGINES)//2 + PIONOWY_MARGINES)
+       
+        #rysowanie planszy pozycyjnej
+        draw_grid(top = (WYSOKOSC-PIONOWY_MARGINES)//2 + PIONOWY_MARGINES)
+        draw_grid(left = (SZEROKOSC-POZIOMY_MARGINES)//2 + POZIOMY_MARGINES)
+        
+
+
+
+
+
+
 
 
         #update screen
